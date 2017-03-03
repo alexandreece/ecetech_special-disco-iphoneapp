d3a6392093a8e78e0022ccc72a7ce06cfeca8353
Adrien <adrien@macbook-air-de-tiptop.local>
2017-03-03 12:38:34 +0100

    Manche 1,2,3 ok avec transition

--- LAMApp/GameScene.swift
+++ LAMApp/GameScene.swift
@@ -17,6 +17,11 @@ class GameScene: SKScene
     
     var groupe1 : SKNode?
     var groupe2 : SKNode?
+    var groupe3 : SKNode?
+    let pointZero = CGPoint(x: 0, y: 0)
+    var nextButton : SKNode?
+    
+    
     
     override func didMove(to view: SKView)
     {
@@ -30,30 +35,59 @@ class GameScene: SKScene
             groupe2 = g2
         }
         
-        groupe1?.isHidden = false
-        groupe2?.isHidden = true
+        if let g3 = childNode(withName: "//TroisiemeManche")
+        {
+            groupe3 = g3
+        }
+        
+        
+        
+        
+        
+        nextButton = childNode(withName: "//NextButton")
+        
+        //nextButton = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
     }
     
     
-
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
     {
-
+        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
     {
-        groupe1?.isHidden = !groupe1!.isHidden
-        groupe2?.isHidden = !groupe2!.isHidden
+        if (groupe1?.position == pointZero){
+            groupe1?.run(SKAction.moveTo(y: -1900, duration: 0.5), completion:
+                {
+                    print("Premiere manche")
+            })
+            groupe2?.run(SKAction.move(to: pointZero, duration: 0.5), completion:
+                {
+                    print("Deuxieme manche")
+            })
+        }
+        
+        if (groupe2?.position == pointZero){
+            groupe2?.run(SKAction.moveTo(y: -1900, duration: 0.5), completion:
+                {
+                    print("Deuxieme manche")
+            })
+            groupe3?.run(SKAction.move(to: pointZero, duration: 0.5), completion:
+                {
+                    print("Troisieme manche")
+            })
+        }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        
     }
     
     
