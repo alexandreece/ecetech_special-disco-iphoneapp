dab6d1a30044a04ed8399dec88b1c9650a802c12
Alexandre <alexandre.suhas@edu.ecetech.fr>
2017-03-31 18:58:06 +0200

    essai

--- DataBase.swift
+++ DataBase.swift
@@ -5,7 +5,6 @@
 //  Created by Alexandre on 03/03/2017.
 //  Copyright © 2017 LAMA. All rights reserved.
 //
-
 import UIKit
 
 protocol DataBaseDelegate {
@@ -15,14 +14,10 @@ protocol DataBaseDelegate {
 
 class PreviousWord : NSObject, NSCoding {
     
-    var idPreviousWord = Int()
     var previousWord = String()
-    
-    static let KeyIdPreviousWord = "KeyIdPreviousWord"
     static let KeyPreviousWord = "KeyPreviousWord"
     
     func encode(with aCoder: NSCoder) {
-        aCoder.encode(idPreviousWord, forKey: PreviousWord.KeyIdPreviousWord)
         aCoder.encode(previousWord, forKey:PreviousWord.KeyPreviousWord)
     }
     
@@ -31,12 +26,13 @@ class PreviousWord : NSObject, NSCoding {
     }
     
     public required init?(coder aDecoder: NSCoder) {
-        if let value = aDecoder.decodeObject(forKey: PreviousWord.KeyIdPreviousWord) as? Int {
-            self.idPreviousWord = value
-        }
         if let value = aDecoder.decodeObject(forKey: PreviousWord.KeyPreviousWord) as? String {
             self.previousWord = value
         }
+    }
+    
+    public init?(previousWord : String) {
+        self.previousWord = previousWord
     }
 }
 
@@ -69,6 +65,70 @@ class Word: NSObject, NSCoding {
         if let value = aDecoder.decodeObject(forKey: Word.KeyWord) as? String {
             self.word = value
         }
+    }
+}
+
+class Score: NSObject, NSCoding {
+    var nomEquipe = String()
+    var score = String()
+    var date = String()
+    var nbPlayer = String()
+    var niveau = String()
+    
+    static let KeyNomEquipe = "KeyNomEquipe"
+    static let KeyScore = "KeyScore"
+    static let KeyDate = "KeyDate"
+    static let KeyNbPlayer = "KeyNbPlayer"
+    static let KeyNiveau = "KeyNiveau"
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(nomEquipe, forKey: Score.KeyNomEquipe)
+        aCoder.encode(score, forKey: Score.KeyScore)
+        aCoder.encode(date, forKey: Score.KeyDate)
+        aCoder.encode(nbPlayer, forKey: Score.KeyNbPlayer)
+        aCoder.encode(niveau, forKey:Score.KeyNiveau)
+    }
+    
+    override init() {
+        super.init()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        if let value = aDecoder.decodeObject(forKey: Score.KeyNomEquipe) as? String {
+            self.nomEquipe = value
+        }
+        if let value = aDecoder.decodeObject(forKey: Score.KeyScore) as? String {
+            self.score = value
+        }
+        if let value = aDecoder.decodeObject(forKey: Score.KeyDate) as? String {
+            self.date = value
+        }
+        if let value = aDecoder.decodeObject(forKey: Score.KeyNbPlayer) as? String {
+            self.nbPlayer = value
+        }
+        if let value = aDecoder.decodeObject(forKey: Score.KeyNiveau) as? String {
+            self.niveau = value
+        }
+    }
+}
+
+class PreviousWordDataBase : NSObject {
+    private static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    private static let ArchiveURL = DocumentsDirectory.appendingPathComponent("PreviousWordsList")
+    static let shared = PreviousWordDataBase()
+    public var delegate : DataBaseDelegate? = nil
+    public var objects = [PreviousWord]()
+    
+    func saveToDisk() -> Bool {
+        return NSKeyedArchiver.archiveRootObject(objects, toFile:PreviousWordDataBase.ArchiveURL.path)
+    }
+    
+    func loadFromDisk() -> Bool {
+        if let data = NSKeyedUnarchiver.unarchiveObject(withFile: PreviousWordDataBase.ArchiveURL.path) as? [PreviousWord] {
+            objects = data
+            return true
+        }
+        return false
     }
 }
 
@@ -140,4 +200,88 @@ class WordDataBase: NSObject {
         return false
     }
     
+}
+
+class ScoreDataBase: NSObject {
+    private static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    private static let ArchiveURL = DocumentsDirectory.appendingPathComponent("ScoresCache")
+    static let shared = ScoreDataBase()
+    public var delegate : DataBaseDelegate? = nil
+    public var objects = [Score]()
+    
+    public func loadData() {
+        let path = "http://78.192.156.30:8050/LAMA/api/index.php/teams/Best"
+        
+        if let url = URL(string: path) {
+            let task = URLSession.shared.dataTask(with: url, completionHandler:
+            { (data, response, error) -> Void in
+                guard let data = data, error == nil else
+                {
+                    return
+                }
+                // ok
+                self.parseJSON(data: data)
+            })
+            task.resume()
+        }
+    }
+    
+    func parseJSON(data : Data) {
+        do {
+            let dataJson = try JSONSerialization.jsonObject(with: data, options: .allowFragments )
+            guard let root = dataJson as? [[String : AnyObject]] else {
+                return
+            }
+            objects.removeAll()
+            
+            for item in root {
+                let dataItem = Score()
+                
+                if let nomEquipeEntry = item["NomTeam"] as? String {
+                    dataItem.nomEquipe = nomEquipeEntry
+                }
+                if let scoreEntry = item["Score"] as? String {
+                    dataItem.score = scoreEntry
+                }
+                if let dateEntry = item["Date"] as? String {
+                    dataItem.date = dateEntry
+                }
+                if let nbPlayerEntry = item["NbPlayer"] as? String {
+                    dataItem.nbPlayer = nbPlayerEntry
+                }
+                if let niveauEntry = item["Niveau"] as? String {
+                    dataItem.niveau = niveauEntry
+                }
+                objects.append(dataItem)
+            }
+            if (self.saveToDisk())
+            {
+                print("Save OK")
+            }
+            else
+            {
+                print("Save ERROR")
+            }
+            DispatchQueue.main.async
+                {
+                    self.delegate?.didLoadData()
+            }
+        }
+        catch {
+            assert( false )
+            return
+        }
+    }
+    
+    func saveToDisk() -> Bool {
+        return NSKeyedArchiver.archiveRootObject(objects, toFile: ScoreDataBase.ArchiveURL.path)
+    }
+    
+    func loadFromDisk() -> Bool {
+        if let data = NSKeyedUnarchiver.unarchiveObject(withFile: ScoreDataBase.ArchiveURL.path) as? [Score] {
+            objects = data
+            return true
+        }
+        return false
+    }
 }
--- LAMApp/Game.swift
+++ LAMApp/Game.swift
@@ -5,12 +5,7 @@
 //  Created by VRSTL on 03/03/2017.
 //  Copyright © 2017 LAMA. All rights reserved.
 //
-
 import Foundation
-
-import GameplayKit
-
-
 
 class Game{
     
@@ -18,12 +13,11 @@ class Game{
     
     // ----- INSTANTIATION ----- //
     var Level: Int
-
-    var NbPlayers: Int
+    
+    var NbPlayers: Int = 5
+    var NbTeam = 2
     
     var TeamA: String
-    
-
     var TeamB: String
     
     var TeamA_List_Joueurs = [Joueur]()
@@ -31,18 +25,14 @@ class Game{
     
     var NbWords: Int
     var CurrentWord: Int
-
-    
-    var posTab = 0
-    
     var Words_List = [String]()
     var Words_Current_List = [String]()
-    //var Word: String
-
-
+    var Word: String
+    var WordRndm: String
+    var WordPlayer: Int
     
-//    int NbPointsTurnTeamA;
-//    int NbPointsTurnTeamB;
+    //    int NbPointsTurnTeamA;
+    //    int NbPointsTurnTeamB;
     var CurrentRound: Int
     var PlayerToPlay: [Int]
     var NbPointsTurn: Int
@@ -52,85 +42,185 @@ class Game{
     
     // ----- CONSTRUCTOR ----- //
     init() {
-    self.Level = 0
-
-    self.NbPlayers = 3
-    self.TeamA = "Lamatraque"
-    self.TeamB = "Lamatrice"
-    self.NbWords = 0
-    
-
-    self.CurrentWord = 0
-    self.CurrentRound = 0
-    self.PlayerToPlay = [0, 0]
-    self.NbPointsTurn = 0
-    self.NbPointsRoundTeamA = 0
-    self.NbPointsRoundTeamB = 0
-
-        
-    self.Words_List = ["papillon", "voiture", "George Cloney", "Obama", "Trump", "Donald Duck"]
-        
-        //copyWordList()
-    self.TeamA_List_Joueurs = [Joueur(),Joueur(),Joueur()]
-    self.TeamB_List_Joueurs = [Joueur(),Joueur(),Joueur()]
-        var i = 0
-        for joueur in TeamA_List_Joueurs {
-            joueur.NomJoueur = "toto \(i)"
-            i += 1
-        }
-        i=0
-        for joueur in TeamB_List_Joueurs {
-            joueur.NomJoueur = "tutu \(i)"
-            i += 1
-        }
-        
-    }/*
-    init() {
         self.Level = 0
-        self.NbPlayers = 0
-        self.TeamA = "Lamatraque"
-        self.TeamB = "Lamatrice"
+        self.NbPlayers = 5
+        self.TeamA = ""
+        self.TeamB = ""
         self.NbWords = 0
         self.Word = ""
+        self.WordRndm = ""
+        self.WordPlayer = 0
         self.CurrentWord = 0
         self.CurrentRound = 0
         self.PlayerToPlay = [0, 0]
         self.NbPointsTurn = 0
         self.NbPointsRoundTeamA = 0
         self.NbPointsRoundTeamB = 0
-        self.Words_List = []
-        self.TeamA_List_Joueurs = [Joueur(),Joueur(),Joueur()]
-        self.TeamB_List_Joueurs = [Joueur(),Joueur(),Joueur()]
-   
-        
-    }*/
+    }
+    
     // ----- GETTERS ----- //
     
-    
-    func getNextWord() ->String
-    {
-         return Words_Current_List[ posTab ]
+    func getLevel() -> Int {
+        return Level
     }
+    
+    func getNbPlayers() -> Int {
+        return NbPlayers
+    }
+    
+    func getNameTeamA() -> String {
+        return TeamA;
+    }
+    
+    func getNameTeamB() -> String {
+        return TeamB;
+    }
+    
+    func getTeamA_List_Joueurs() -> [Joueur] {
+        return TeamA_List_Joueurs
+    }
+    
+    func getTeamB_List_Joueurs() -> [Joueur] {
+        return TeamB_List_Joueurs
+    }
+    
     func getNameJoueurTeamA (pIDJoueur: Int) -> String {
         let Joueur: Joueur = TeamA_List_Joueurs[pIDJoueur]
-        let JoueurName: String = Joueur.NomJoueur
-
+        let JoueurName: String = Joueur.GetNomJoueur()
         return JoueurName
     }
     
     func getNameJoueurTeamB (pIDJoueur: Int) -> String {
         let Joueur: Joueur = TeamB_List_Joueurs[pIDJoueur]
-
-        let JoueurName: String = Joueur.NomJoueur
+        let JoueurName: String = Joueur.GetNomJoueur()
         return JoueurName
     }
     
-    func copyWordList()
-    {
-    
-        Words_Current_List = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: Words_List) as! [String]
-        
+    func getCurrentWord() -> Int {
+        return CurrentRound
     }
-   
-
+    
+    func getNbWords() -> Int{
+        return NbWords
+    }
+    
+    func getWords_List() -> [String] {
+        return Words_List
+    }
+    
+    func getWords_Current_List() -> [String] {
+        return Words_Current_List
+    }
+    
+    func getWord (pWordID: Int) -> String {
+        return Words_List[pWordID]
+    }
+    
+    func getWordCurrentList (pWordID: Int) -> String {
+        return Words_Current_List[pWordID]
+    }
+    
+    func getPlayerToPlay() -> [Int] {
+        return PlayerToPlay
+    }
+    
+    func getCurrentRound() -> Int {
+        return CurrentRound
+    }
+    
+    func getNbPointsTurn() -> Int {
+        return NbPointsTurn
+    }
+    
+    func getNbPointsRoundTeamA() -> Int {
+        return NbPointsRoundTeamA
+    }
+    
+    func getNbPointsRoundTeamB() -> Int {
+        return NbPointsRoundTeamB
+    }
+    
+    
+    // ----- SETTERS ----- //
+    
+    func setLevel (pLevel: Int) {
+        self.Level = pLevel
+    }
+    
+    func setNbPlayers (pNbPlayers: Int) {
+        self.NbPlayers = pNbPlayers
+    }
+    
+    func setNameTeamA (pNameTeamA: String) {
+        self.TeamA = pNameTeamA
+    }
+    
+    func setNameTeamB (pNameTeamB: String) {
+        self.TeamB = pNameTeamB
+    }
+    
+    func addTeamA (TeamA: [Joueur]) {
+        self.TeamA_List_Joueurs = TeamA
+    }
+    
+    func addTeamB (TeamB: [Joueur]) {
+        self.TeamB_List_Joueurs = TeamB
+    }
+    
+    func addPlayerTeamA (pJoueur: Joueur) {
+        self.TeamA_List_Joueurs.append(pJoueur)
+    }
+    
+    func addPlayerTeamB (pJoueur: Joueur) {
+        self.TeamB_List_Joueurs.append(pJoueur)
+    }
+    
+    func setCurrentWord (pCurrentWord: Int) {
+        self.CurrentRound = pCurrentWord
+    }
+    
+    func setNbWords (pNbWords: Int) {
+        self.NbWords = pNbWords
+    }
+    
+    func setWords_List (pWordsList: [String]) {
+        self.Words_List = pWordsList
+    }
+    
+    func addWord (pWord: String) {
+        self.Words_List.append(pWord)
+    }
+    
+    func deleteWord (pLastWordID: Int) {
+        self.Words_Current_List.remove(at: pLastWordID)
+    }
+    
+    func setCurrentRound (pCurrentRound: Int) {
+        self.CurrentRound = pCurrentRound
+    }
+    
+    func setPlayerToPlay (pPlayerToPlay: [Int]) {
+        self.PlayerToPlay = pPlayerToPlay
+    }
+    
+    func setNbPointsTurn (pNbPoints: Int) {
+        self.NbPointsTurn = pNbPoints
+    }
+    
+    /*
+     public void setNbPointsTurnTeamA(int pNbPointsTurn){
+     NbPointsTurnTeamA = pNbPointsTurn;
+     }
+     public void setNbPointsTurnTeamB(int pNbPointsTurn){
+     NbPointsTurnTeamB = pNbPointsTurn;
+     }
+     */
+    
+    func setNbPointsRoundTeamA (pNbPointsRound: Int) {
+        self.NbPointsRoundTeamA = pNbPointsRound
+    }
+    
+    func setNbPointsRoundTeamB (pNbPointsRound: Int) {
+        self.NbPointsRoundTeamB = pNbPointsRound
+    }
 }
